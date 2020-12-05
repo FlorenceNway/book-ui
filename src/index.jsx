cb086718699bb4ac642e86ae416ddc56b389b31e
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-05 09:45:13 +0000

    antd.css import

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
-import 'semantic-ui-css/semantic.min.css';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>

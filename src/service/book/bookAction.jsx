c10e25ae0dad58e2b123d14af60808b23a7c5aba
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-06 09:30:04 +0000

    api path changed

--- src/service/book/bookAction.jsx
+++ src/service/book/bookAction.jsx
@@ -1,10 +1,9 @@
-import { CREATE_BOOK } from './bookActionTypes';
-import { GET_ALL_BOOKS } from './bookActionTypes';
 import axios from 'axios';
+import { CREATE_BOOK, GET_ALL_BOOKS } from './bookActionTypes';
 
 export const getAllBooks = () => async (dispatch) => {
   try {
-    const response = await axios.get('http://localhost:5001/books');
+    const response = await axios.get('http://localhost:5001/api/books');
 
     dispatch({
       type: GET_ALL_BOOKS,

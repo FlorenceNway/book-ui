3690bb9bd7b86415d93bbdc5620dc9a1ca10b1ba
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-12 18:14:04 +0000

    added create delete update actions

--- src/service/book/bookAction.jsx
+++ src/service/book/bookAction.jsx
@@ -1,5 +1,25 @@
 import axios from 'axios';
-import { CREATE_BOOK, GET_ALL_BOOKS } from './bookActionTypes';
+import {
+  CREATE_BOOK,
+  GET_ALL_BOOKS,
+  DELETE_BOOK,
+  UPDATE_BOOK,
+} from './bookActionTypes';
+
+export const createBook = (data, onSussess) => async (dispatch) => {
+  try {
+    const response = await axios.post('http://localhost:5001/api/books', data);
+
+    dispatch({
+      type: CREATE_BOOK,
+    });
+    if (typeof onSussess === 'function') {
+      onSussess();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getAllBooks = () => async (dispatch) => {
   try {
@@ -8,6 +28,34 @@ export const getAllBooks = () => async (dispatch) => {
     dispatch({
       type: GET_ALL_BOOKS,
       payload: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteBook = (id, onSuccess) => async (dispatch) => {
+  try {
+    await axios.delete(`http://localhost:5001/api/books/${id}`);
+    if (typeof onSuccess === 'function') {
+      onSuccess();
+    }
+    dispatch({
+      type: DELETE_BOOK,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateBook = (id, data, onSuccess) => async (dispatch) => {
+  try {
+    await axios.put(`http://localhost:5001/api/books/${id}`, data);
+    if (typeof onSuccess === 'function') {
+      onSuccess();
+    }
+    dispatch({
+      type: UPDATE_BOOK,
     });
   } catch (error) {
     console.log(error);

6aeb81150405d554dcacb7de52ee9c29a6b34771
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-13 10:03:52 +0000

    modal for edit btn

---
+++ src/pages/book/components/BookUpdateModal.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import _ from 'lodash';
+import { Modal, Form, Input, notification } from 'antd';
+import { updateBook, getAllBooks } from '../../../service/book/bookAction';
+
+const { TextArea } = Input;
+
+const BookUpdateModal = ({ isModalVisible, handleCancel, initialValue }) => {
+  const [name, setName] = useState();
+  const [pages, setPages] = useState();
+  const [description, setDescription] = useState();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setName(initialValue?.name);
+    setPages(initialValue?.pages);
+    setDescription(initialValue?.description);
+  }, []);
+
+  const openNotification = () => {
+    notification.open({
+      message: 'Updated Success',
+      description: 'Updated',
+      onClick: () => {
+        console.log('Notification Clicked!');
+      },
+    });
+  };
+  const handleOk = () => {
+    const id = initialValue.key;
+    const data = {
+      name,
+      pages: _.parseInt(pages),
+      description,
+    };
+    dispatch(
+      updateBook(id, data, () => {
+        dispatch(getAllBooks());
+        openNotification();
+      })
+    );
+    handleCancel();
+  };
+
+  return (
+    <Modal
+      title="New Book"
+      visible={isModalVisible}
+      onOk={handleOk} // when server received data
+      onCancel={handleCancel}
+    >
+      <Form name="basic" initialValues={initialValue}>
+        <Form.Item
+          label="Name"
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your book name!',
+            },
+          ]}
+        >
+          <Input onChange={(event) => setName(event.target.value)} />
+        </Form.Item>
+        <Form.Item
+          label="Pages"
+          name="pages"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your book pages!',
+            },
+          ]}
+        >
+          <Input onChange={(event) => setPages(event.target.value)} />
+        </Form.Item>
+        <Form.Item
+          label="Description"
+          name="description"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your book Description!',
+            },
+          ]}
+        >
+          <TextArea onChange={(event) => setDescription(event.target.value)} />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+export default BookUpdateModal;

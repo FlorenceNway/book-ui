f5998563b3fbd5e51a0af40913033055d2090b65
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-13 10:04:31 +0000

    landing page

--- src/pages/book/Books.jsx
+++ src/pages/book/Books.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect } from 'react';
+import { Button, Divider } from 'antd';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import CustomLayout from '../../components/customLayout/CustomLayout';
 import Paper from '../../components/paper/Paper';
 import { getAllBooks } from '../../service/book/bookAction';
+import BookTable from './components/BookTable';
+import styles from './books.module.scss';
+import BookNewModal from './components/BookNewModal';
 
 const Books = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,7 +18,19 @@ const Books = () => {
 
   return (
     <CustomLayout>
-      <Paper>Hello</Paper>
+      <Paper>
+        <div className={styles.btnNew}>
+          <Button type="primary" onClick={() => setIsModalVisible(true)}>
+            New
+          </Button>
+        </div>
+        <Divider />
+        <BookTable />
+      </Paper>
+      <BookNewModal
+        isModalVisible={isModalVisible}
+        handleCancel={() => setIsModalVisible(false)}
+      />
     </CustomLayout>
   );
 };

16cf169c29de480983fc33b9257f097515e2adc8
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-15 22:49:01 +0000

    edit route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Books from '../pages/book/Books';
+import BookUpdate from '../pages/book/BookUpdate';
 import store from '../store/store';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Switch>
+          <Route path="/edit/:id" component={BookUpdate} />
           <Route path="/" component={Books} />
         </Switch>
       </Router>

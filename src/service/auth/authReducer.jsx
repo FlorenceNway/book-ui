194a093484e452bcc19e66d5c3907d92b5a6cafb
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-20 11:55:35 +0000

    authReducer init

---
+++ src/service/auth/authReducer.jsx
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+const initialValues = {
+  data: {},
+};
+
+export default function (state = initialValues, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}

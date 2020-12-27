98e3e2005b6393b8a56f7b4f77a81fa105c7644e
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-27 10:03:17 +0000

    auth reducer

--- src/store/combineReducer.jsx
+++ src/store/combineReducer.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import bookReducer from '../service/book/bookReducer';
+import authReducer from '../service/auth/authReducer';
 
 const rootReducer = combineReducers({
   bookReducer,
+  authReducer,
 });
 
 export default rootReducer;

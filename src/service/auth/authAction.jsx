9286b6e22687243168db0fcd4252c896e20a59f0
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-29 23:15:41 +0000

    login action

--- src/service/auth/authAction.jsx
+++ src/service/auth/authAction.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-console */
 import axios from 'axios';
-import { REGISTER } from './authActionTypes';
+import { REGISTER, LOGIN } from './authActionTypes';
 
 export const registerUser = (data, onSuccess) => async (dispatch) => {
   try {
@@ -12,6 +13,25 @@ export const registerUser = (data, onSuccess) => async (dispatch) => {
       onSuccess();
     }
   } catch (error) {
-    console.log(error.response); //error.response.data
+    console.log(error.response); // error.response.data
+  }
+};
+
+export const loginUser = (data, onSuccess) => async (dispatch) => {
+  try {
+    const response = await axios.post(
+      'http://localhost:5001/api/users/login',
+      data
+    );
+
+    dispatch({
+      type: LOGIN,
+      payload: response.data,
+    });
+    if (typeof onSuccess === 'function') {
+      onSuccess();
+    }
+  } catch (error) {
+    console.log(error.response); // error.response.data
   }
 };

1f96c8a2450137ed16c05edeca647f7bf8259724
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-30 23:54:43 +0000

    set token

--- src/service/auth/authReducer.jsx
+++ src/service/auth/authReducer.jsx
@@ -1,11 +1,16 @@
 import _ from 'lodash';
+import { LOGIN } from './authActionTypes';
 
 const initialValues = {
-  data: {},
+  data: { isAuthenticated: false },
 };
 
 export default function (state = initialValues, action) {
   switch (action.type) {
+    case LOGIN:
+      localStorage.setItem('TOKEN', action.payload);
+      // store in localstorage when we get token after login
+      return { isAuthenticated: action.payload ? true : false };
     default:
       return state;
   }
